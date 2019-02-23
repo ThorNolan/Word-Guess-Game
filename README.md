8e45e6e53e7e613b554c055f1558335589e1b659
Thor Nolan <tnolan5764@gmail.com>
2019-02-22 19:50:11 -0800

    added function to keep track of user guesses and started on my onkeyup events

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Word Guess Game
 =============================
-A simple hangman style game that I made, the theme is Game of Thrones. The word will be randomly chosen (think Game of Thrones characters!) and you have to guess its letters.
+A simple hangman style game that I made, the theme is Game of Thrones. The word will be randomly chosen (think Game of Thrones characters and places!) and you have to guess its letters.
 
 ## How to play
 To play, simply enter in a letter with your keyboard, and the game will tell you whether the letter you guessed was a part of the word or not and adjust your score accordingly. If you run out of guesses, you lose! 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,50 @@
 // VARIABLES
-
-    // array to store possible words
+    // array to store all possible words
     var randomWordArray = [
-        "cersei" , "tyrion" , "tywin", "stark" , "lannister" , "littlefinger" , "baratheon" , "daenerys" , "joffrey" , "ygritte" , "hodor" , "greyjoy" , "theon" , "tyrell"
+        "cersei" , "tyrion" , "tywin", "stark" , "lannister" , "littlefinger" , "baratheon" , "daenerys" , "joffrey" , "ygritte" , "hodor" , "greyjoy" , "theon" , "tyrell" , "dorne" , "bravos" , "highgarden" , "direwolf" , "dragon" , "giants" , "wights" , "walkers"
     ];
 
-    // variable to pick a random word from the random word array
+    // variable to pick a random word from the randomWordArray
     var randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
 
-    // variables to keep track of score and an empty array
-    var s;
-    var count = 0;
-    var answerArray = [];
+    // variables to keep track of score and an empty array for keeping track of user input
+    var string;
+    var guessCounter = 0;
+    var answers = [];
+    var lives = 9;
 
 
-//FUNCTIONS
+// FUNCTIONS
 
-    function startGame() {
+    // function to put underscores in the answerArray based on the length of the word that needs to be guessed
+    window.onload = function startGame() {
         for (var i = 0; i < randomWord.length; i++)
         {
-            answerArray[i] = "_"; 
+            answers[i] = "_"; 
+        }
+ 
+        string = answers.join(" ");
+        document.getElementById("guessWord").innerHTML = string;
+    }
+
+
+    window.onload = function userGuess() {
+        var guess = document.getElementById("lettersGuessed").value;
+
+        //make another check here, maybe against an alphabet array to make sure user input is a letter and not a number or symbol
+        if (guess.length > 0) {
+            
+            for (var i = 0; i < randomWord.length; i++){
+                if (randomWord[i] === guess) {
+                    answers[i] = guess;
+                }   
+            }
+
+            guessCounter++;
+            document.getElementById("guessesRemaining").innerHTML = "Your guesses: " + guessCounter;
+            document.getElementById("answers").innerHTML = answers.join("");
         }
 
-        s = answerArray.join(" ");
-        document.getElementById("guessWord").innerHTML = s;
+        //another if statement here to say something (or add an image) based on number of guesses
+
     }
-
-function userGuess() {
-    var guess = document.getElementById("lettersGuessed").value;
-
-    if (guess.length > 0) {
-       for (var i = 0; i < randomWord.length; i++){
-
-       } 
-    }
-
-}
-
-
-
-
-
-// GAMEPLAY PROCESS
-
-startGame()
